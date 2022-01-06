e673af806cef3acc130e37ff8770baf8c0f8b5ea
Ark223 <Ark223Neww@gmail.com>
2022-01-06 22:31:48 +0100

    Initial commit

--- src/main/java/pl/edu/pbs/sklep/ShopApplication.java
+++ src/main/java/pl/edu/pbs/sklep/ShopApplication.java
@@ -9,7 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @EntityScan("pl.edu.pbs.sklep.model.entities")
-@ComponentScan({"pl.edu.pbs.sklep.repository"})
+@ComponentScan({"pl.edu.pbs.sklep.repository", "pl.edu.pbs.sklep.service", "pl.edu.pbs.sklep.controller"})
 public class ShopApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
